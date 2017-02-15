9ef54389a009bc3c2befe8b1134de19f006331bb
libunamari <marialibunao@gmail.com>
2017-02-14 17:03:48 -0800

    added comments to httpserver

--- Ryu_Application/HTTPServer.py
+++ Ryu_Application/HTTPServer.py
@@ -3,23 +3,32 @@ from SocketServer import ThreadingMixIn
 
 import os
 import signal
-import sys
 import lockfile
-import psutil
 import json
 import cgi
 
 CAPFLOW = "/v1.1/authenticate/auth"
 AUTH_PATH = "/authenticate/auth"
 IDLE_PATH = "/idle"
-class HTTPHandler(BaseHTTPRequestHandler):
 
-    _contr_pid = -1 #the process ID of the controller
-    dot1x_active_file = os.getenv('DOT1X_ACTIVE_HOSTS', '/etc/ryu/1x_active_users.txt')
-    dot1x_idle_file = os.getenv('DOT1X_IDLE_HOSTS', '/etc/ryu/1x_idle_users.txt')
-    capflow_file = os.getenv('CAPFLOW_CONFIG','/etc/ryu/capflow_config.txt')
-        
-    def _set_headers(self,code,ctype):
+
+class HTTPHandler(BaseHTTPRequestHandler):
+    '''
+    This class receives HTTP messages from the portal about
+    the a change of state of the users.
+    This could be either a log on or a log off of a user.
+    The information is then passed to the controller by
+    modifying configuration files as well as sending a signal to it.
+    '''
+
+    _contr_pid = -1  #the process ID of the controller
+    dot1x_active_file = os.getenv('DOT1X_ACTIVE_HOSTS',
+                                  '/etc/ryu/1x_active_users.txt')
+    dot1x_idle_file = os.getenv('DOT1X_IDLE_HOSTS',
+                                '/etc/ryu/1x_idle_users.txt')
+    capflow_file = os.getenv('CAPFLOW_CONFIG', '/etc/ryu/capflow_config.txt')
+
+    def _set_headers(self, code, ctype):
         self.send_response(code)
         self.send_header('Content-type', ctype)
         self.end_headers()
@@ -28,105 +37,111 @@ class HTTPHandler(BaseHTTPRequestHandler):
         json_data = self.check_if_json()
         if json_data == None:
             return
-            
+
         if self.path == AUTH_PATH or self.path == CAPFLOW:
             self.authenticate(json_data)
         elif self.path == IDLE_PATH:
-            self.idle(json_data)        
+            self.idle(json_data)
         else:
             self.send_error('Path not found\n')
-            
-   
+
     def do_DELETE(self):
         json_data = self.check_if_json()
         if json_data == None:
             return
-            
+
         if self.path == AUTH_PATH:
             #check json has the right information
             if not (json_data.has_key("mac") and json_data.has_key("user")):
                 self.send_error('Invalid form\n')
                 return
-            self.deauthenticate(self.dot1x_active_file, json_data["mac"], signal.SIGUSR1)
+            self.deauthenticate(self.dot1x_active_file, json_data["mac"],
+                                signal.SIGUSR1)
         elif self.path == CAPFLOW:
             #check json has the right information
-            if not (json_data.has_key("ip")):
+            if not json_data.has_key("ip"):
                 self.send_error('Invalid form\n')
                 return
-            self.deauthenticate(self.capflow_file, json_data["ip"], signal.SIGUSR2)
+            self.deauthenticate(self.capflow_file, json_data["ip"],
+                                signal.SIGUSR2)
         else:
             self.send_error('Path not found\n')
-            
-    
+
     def authenticate(self, json_data):
-        if self.path == AUTH_PATH: #request is for dot1xforwarder
+        if self.path == AUTH_PATH:  #request is for dot1xforwarder
             if not (json_data.has_key("mac") and json_data.has_key("user")):
                 self.send_error('Invalid form\n')
-                return            
-            
+                return
+
             #valid request format so new user has authenticated
-            self.write_to_file(self.dot1x_active_file, json_data["mac"], json_data["user"])
+            self.write_to_file(self.dot1x_active_file, json_data["mac"],
+                               json_data["user"])
             self.send_signal(signal.SIGUSR1)
-            message = "authenticated new client({}) at MAC: {}\n".format(json_data["user"], json_data["mac"]) 
-        
-        else: #request is for CapFlow
+            message = "authenticated new client({}) at MAC: {}\n".format(
+                json_data["user"], json_data["mac"])
+
+        else:  #request is for CapFlow
             if not (json_data.has_key("ip") and json_data.has_key("user")):
                 self.send_error('Invalid form\n')
                 return
-            
+
             #valid request format so new user has authenticated
-            self.write_to_file(self.capflow_file, json_data["ip"], json_data["user"])
+            self.write_to_file(self.capflow_file, json_data["ip"],
+                               json_data["user"])
             self.send_signal(signal.SIGUSR2)
-            message = "authenticated new client({}) at IP: {}\n".format(json_data["user"], json_data["ip"]) 
-        
-        #write response 
-        self._set_headers(200, 'text/html') 
+            message = "authenticated new client({}) at IP: {}\n".format(
+                json_data["user"], json_data["ip"])
+
+        #write response
+        self._set_headers(200, 'text/html')
         self.wfile.write(message)
-        self.log_message("%s",message)
-        
+        self.log_message("%s", message)
+
     def idle(self, json_data):
         if not (json_data.has_key("mac") and json_data.has_key("retrans")):
             self.send_error("Invalid form\n")
             return
-        
-        self.write_to_file(self.dot1x_idle_file, json_data["mac"], json_data["retrans"])
+
+        self.write_to_file(self.dot1x_idle_file, json_data["mac"],
+                           json_data["retrans"])
         self.send_signal(signal.SIGUSR1)
         self._set_headers(200, 'text/html')
-        message = "Idle user on {} has been made to use captive portal after {} retransmissions\n".format(json_data["mac"], json_data["retrans"])
-        self.log_message("%s",message)
+        message = "Idle user on {} has been made to use captive portal after {} retransmissions\n".format(
+            json_data["mac"], json_data["retrans"])
+        self.log_message("%s", message)
         self.wfile.write(message)
-        
+
     def deauthenticate(self, filename, unique_identifier, signal_type):
         fd = lockfile.lock(filename, os.O_APPEND | os.O_WRONLY)
         changed, to_write = self.read_file(filename, unique_identifier)
-        
-        if changed: #user has been deleted, update the file
-            os.ftruncate(fd,0) #clear the file
-            os.write(fd, to_write) 
+
+        if changed:  #user has been deleted, update the file
+            os.ftruncate(fd, 0)  #clear the file
+            os.write(fd, to_write)
         lockfile.unlock(fd)
         self.send_signal(signal_type)
-        
+
         self._set_headers(200, 'text/html')
         message = "deauthenticated client at {} \n".format(unique_identifier)
         self.wfile.write(message)
-        self.log_message("%s",message)
-        
+        self.log_message("%s", message)
+
     def write_to_file(self, filename, str1, str2):
         ''' Write two strings which are comma separated, to a file
-        
+
         :param filename: the name of the file we are writing to
         :param str1: the first string
-        :param str2: the second string        
+        :param str2: the second string
         '''
         #try to obtain lock to prevent concurrent access
-        fd = lockfile.lock(filename, os.O_APPEND | os.O_WRONLY) 
+        fd = lockfile.lock(filename, os.O_APPEND | os.O_WRONLY)
         string = str(str1) + "," + str(str2) + "\n"
         os.write(fd, string)
         lockfile.unlock(fd)
-    
-    def read_file(self,filename, unique_identifier):
+
+    def read_file(self, filename, unique_identifier):
         ''' Read a file and delete entries which contain the unique identifier
-        
+
         :param filename: the name of the file
         :param unique_identifier: the entry which will be deleted
         :return: A tuple which contains a boolean of whether or not the unique 
@@ -137,33 +152,37 @@ class HTTPHandler(BaseHTTPRequestHandler):
         file_changed = False
         with open(filename) as file_:
             for line in file_:
-                unique_identifier1, user1= line.split(",")
+                unique_identifier1, user1 = line.split(",")
                 if unique_identifier != unique_identifier1:
                     to_write += line
                 else:
                     file_changed = True
 
         return file_changed, to_write
-    
+
     def send_signal(self, signal_type):
         ''' Send a signal to the controller to indicate a change in config file
-        
+
         :param signal_type: SIGUSR1 for dot1xforwarder, SIGUSR2 for CapFlow
         '''
-        if self._contr_pid < 0: #has not looked up controller process ID yet
-            for process in psutil.process_iter():
-                if process.name() == "ryu-manager" and any("controller.py" in s for s in process.cmdline()):
-                    self._contr_pid = process.pid
-                    break
-        os.kill(self._contr_pid,signal_type)  
-    
+        #if self._contr_pid < 0:  #has not looked up controller process ID yet
+            #for process in psutil.process_iter():
+                #if process.name() == "ryu-manager" and any(
+                        #"controller.py" in s for s in process.cmdline()):
+                    #self._contr_pid = process.pid
+                    #break
+        with open(os.path.expanduser("~/controller_pid.txt"), "r") as f:
+            self._contr_pid = int(f.read())
+        os.kill(self._contr_pid, signal_type)
+
     def check_if_json(self):
         try:
-            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+            ctype, pdict = cgi.parse_header(
+                self.headers.getheader('content-type'))
         except:
             self.send_error("No content-type header\n")
             return None
-            
+
         if ctype != 'application/json':
             self.send_error("Data is not a JSON object\n")
             return None
@@ -172,24 +191,24 @@ class HTTPHandler(BaseHTTPRequestHandler):
         try:
             json_data = json.loads(data)
         except ValueError:
-             self.send_error("Not JSON object\n")
-             return None
-        
+            self.send_error("Not JSON object\n")
+            return None
+
         return json_data
-    
-    def send_error(self,error):
+
+    def send_error(self, error):
         self._set_headers(404, 'text/html')
         self.log_message("Error: %s", error)
-        self.wfile.write(error) 
-        
+        self.wfile.write(error)
+
     do_GET = do_POST
-        
-    
+
+
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     pass
-    
+
+
 if __name__ == "__main__":
     server = ThreadedHTTPServer(('', 8080), HTTPHandler)
     print "starting server"
     server.serve_forever()
-    
